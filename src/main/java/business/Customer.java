794708f1bc2edb46a82394b4ebce048d69e7cb74
Zain-MKhan <zmkhan025@gmail.com>
2023-10-13 04:04:36 -0400

    initial business layer implementation

---
+++ src/main/java/business/Customer.java
@@ -0,0 +1,35 @@
+package business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer extends User {
+    private List<Product> shoppingCart;  // List of products in the shopping cart
+
+    // Constructor
+    public Customer(long userId, String username, String email, String password) {
+        super(userId, username, email, password);  // Call the superclass (User) constructor
+        shoppingCart = new ArrayList<>();  // Initialize an empty shopping cart
+    }
+
+    // Getters and setters for customer-specific attributes
+    public List<Product> getShoppingCart() {
+        return shoppingCart;
+    }
+
+    // Customer-specific methods
+    public void addToCart(Product product) {
+        shoppingCart.add(product);
+    }
+
+    public void removeFromCart(Product product) {
+        shoppingCart.remove(product);
+    }
+
+    public void adjustQuantityInCart(Product product, int newQuantity) {
+        // Implement logic to adjust the quantity of items in the cart
+        // For example, update the quantity of the specified product in the cart
+    }
+
+    // Additional customer-specific methods can be added as needed
+}
---
+++ src/main/java/business/User.java
@@ -0,0 +1,91 @@
+package business;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+public class User {
+    private long userId;        // Unique identifier for the user
+    private String username;    // User's username
+    private String email;       // User's email address
+    private String password;    // User's password (hash or encrypted)
+    
+    // Constructor
+    public User(long userId, String username, String email, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    // public Product getProduct(String sku) {
+    //     // Replace "products.json" with the path to your local JSON file
+    //     String jsonFilePath = "products.json";
+
+    //     try {
+    //         // Read the JSON data from the file
+    //         FileReader fileReader = new FileReader(jsonFilePath);
+    //         List<Product> products = new Gson().fromJson(fileReader, new TypeToken<List<Product>>() {
+    //         }.getType());
+
+    //         // Search for the product by SKU
+    //         for (Product product : products) {
+    //             if (product.getSku().equals(sku)) {
+    //                 return product; // Return the product if found
+    //             }
+    //         }
+
+    //         // Handle the case when the product with the given SKU is not found
+    //         // You can throw an exception or return null, depending on your application's design
+    //         return null;
+    //     } catch (IOException e) {
+    //         // Handle any potential IO errors (e.g., file not found, JSON parsing issues)
+    //         e.printStackTrace();
+    //         return null;
+    //     }
+    // }
+
+
+    // Getters and setters for attributes
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Additional methods for user-related functionality
+    public boolean authenticate(String inputPassword) {
+        // Implement user authentication logic (e.g., compare inputPassword with stored password)
+        return this.password.equals(inputPassword);
+    }
+
+    // Other common user-related methods can be added as needed
+}
